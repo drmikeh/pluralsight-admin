a4d58bf1dc5dc068083ff92918ad3446ca4fe7ed
Dr. Mike Hopper <drmikeh@gmail.com>
2017-05-27 12:03:13 -0400

    Fixed dependency versions.

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const HomePage = () => {
-  return (
+const HomePage = () =>
+  (
     <div className="jumbotron">
       <h1>Pluralsight Administration</h1>
       <p>React, Redux, and React Router in ES6 for ultra-responsive web apps.</p>
       <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
     </div>
   );
-};
 
 export default HomePage;
