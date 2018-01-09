3fff4562a72c4658a1106b1726952bfa6597378e
Mike Hopper <drmikeh@gmail.com>
2018-01-09 11:30:41 -0500

    Updated packages.

--- src/components/course/CourseListRow.jsx
+++ src/components/course/CourseListRow.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const CourseListRow = ({ course }) => (
   <tr>
     {/* <td><a href={course.watchHref} target="_blank">Watch</a></td> */}
-    <td><Link to={`/courses/ ${course.id}`}>{course.title}</Link></td>
+    <td><Link to={`/courses/${course.id}`}>{course.title}</Link></td>
     <td>{course.authorId}</td>
     <td>{course.category}</td>
     <td>{course.length}</td>
